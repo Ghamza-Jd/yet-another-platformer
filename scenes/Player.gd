7c2954961ddf008be5e196e84c8e310e4cd0752b
Hamza Jadid <haljadid@proximie.com>
2023-03-20 21:59:57 +0200

    :sparkles: feat: delta time based lerping

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,28 +1,27 @@
 extends CharacterBody2D
 
-var m_gravity: float
-var m_velocity: Vector2
-var m_max_horizontal_speed: float
-var m_jump_speed: float
+var m_gravity: float = 1_000
+var m_velocity: Vector2 = Vector2.ZERO
+var m_max_horizontal_speed: float = 125
+var m_jump_speed: float = 360
+var m_horizontal_acceleration: float = 2_000
 
 func _ready():
-	m_gravity = 300.0
-	m_velocity = Vector2.ZERO
-	m_max_horizontal_speed = 100.0
-	m_jump_speed = 200.0
+	pass
 
 func _process(delta: float):
 	var move_vec = Vector2.ZERO
-	move_vec.x = Input.get_action_strength("move_right") \
-		- Input.get_action_strength("move_left")
+	move_vec.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	move_vec.y = -1 if Input.is_action_just_pressed("jump") else 0
 	
-	m_velocity.x = move_vec.x * m_max_horizontal_speed
-	m_velocity.y += m_gravity * delta
+	m_velocity.x += move_vec.x * m_horizontal_acceleration * delta
+	if (move_vec.x == 0):
+		m_velocity.x = lerp(0.0, m_velocity.x, pow(2, -50 * delta))
+	m_velocity.x = clamp(m_velocity.x, -m_max_horizontal_speed, m_max_horizontal_speed)
+
 	if (move_vec.y < 0 and is_on_floor()):
 		m_velocity.y = move_vec.y * m_jump_speed
 	
+	m_velocity.y += m_gravity * delta
 	velocity = m_velocity
-	var has_collided = move_and_slide()
-	if (has_collided):
-		m_velocity = Vector2.ZERO
+	move_and_slide()
